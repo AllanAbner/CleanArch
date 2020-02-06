8e49f8d6390eb8d614ff739e776c2db4687cc06d
allanabner <allanabner_ti@hotmail.com>
2020-02-05 23:57:20 -0300

    Ioc in presentation Layer

--- CleanArch/CleanArch.MVC/Startup.cs
+++ CleanArch/CleanArch.MVC/Startup.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
+using CleanArch.MVC.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using CleanArch.MVC.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using CleanArch.Infra.Data.Context;
 
 namespace CleanArch.MVC
 {
@@ -48,6 +44,11 @@ namespace CleanArch.MVC
                   Configuration.GetConnectionString("UniversityConnection")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ CleanArch/IOC/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.Serices;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+
+        }
+    }
+}
